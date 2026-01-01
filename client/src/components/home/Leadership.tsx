79d65a1b6639e4e1edc07358078e21d26265127a
hilla10 <hillaman592@gmail.com>
2026-01-01 20:42:27 +0300

    `Fixed typo in Leadership component: "leader" -> "leaders"`

--- client/src/components/home/Leadership.tsx
+++ client/src/components/home/Leadership.tsx
@@ -5,20 +5,19 @@ const Leadership = () => {
     <div className='leadership relative'>
       {/* Content Overlay */}
       <div className='content'>
-        {' '}
         <p>
           Working since <span className='font-bold text-red-600'>2003</span>
           <span />
         </p>
         <h1>
-          We are leader <br /> in Car Mechanical Work
+          We are leaders <br /> in Car Mechanical Work
         </h1>
         <div className='link'>
           <button>
             <img src={play} alt='play icon' />
           </button>
           <span>
-            Watch into video <br /> about us
+            Watch a video <br /> about us
           </span>
         </div>
       </div>
