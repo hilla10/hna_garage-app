80190c774767d1097b760c545021cf42f40ce211
hilla10 <hillaman592@gmail.com>
2026-01-01 15:20:11 +0300

    Change Spelling Error

--- client/src/components/home/Workshop.tsx
+++ client/src/components/home/Workshop.tsx
@@ -35,7 +35,7 @@ const Workshop = () => {
           <p>
             Capitalize on low hanging fruit to identify a ballpark value added
             activity to beta test. Override the digital divide with additional
-            click troughs from DevOps. Nanotechnology immersion along the
+            click throughs from DevOps. Nanotechnology immersion along the
             information highway will close the loop on focusing
           </p>
 
