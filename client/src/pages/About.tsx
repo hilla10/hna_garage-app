7a1d13d42122fa8575dbdfe8a261723bccbc7f3a
hilla10 <hillaman592@gmail.com>
2026-01-03 18:14:21 +0300

    `Refactored CSS and components in client/src`

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,10 +1,20 @@
 import { Hero, MechanicExpertise } from '@components/about';
+import {
+  AppointmentCTA,
+  Leadership,
+  WhyChooseUs,
+  Workshop,
+} from '@components/home';
 
 const About = () => {
   return (
     <section className='about'>
       <Hero />
       <MechanicExpertise />
+      <Workshop />
+      <WhyChooseUs />
+      <Leadership />
+      <AppointmentCTA />
     </section>
   );
 };
